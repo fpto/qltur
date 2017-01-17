b62b1f9d313168e4ed378ce7906fe96f101903f9
Fabricio Puerto <fabricio.puerto@outlook.com>
2017-01-17 22:02:02 +0100

    Created table for concerts and seeded it

--- db/migrate/20170117202727_create_concerts.rb
+++ db/migrate/20170117202727_create_concerts.rb
@@ -1,7 +1,12 @@
 class CreateConcerts < ActiveRecord::Migration
   def change
     create_table :concerts do |t|
-
+      t.text :title
+      t.text :time
+      t.text :date
+      t.text :venue
+      t.text :band
+      t.text :description	
       t.timestamps null: false
     end
   end
