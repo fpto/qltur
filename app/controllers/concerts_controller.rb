2e868e22ea137f9a47d77f127df7ef466498da82
Fabricio Puerto <fabricio.puerto@outlook.com>
2017-01-18 22:51:41 +0100

    Set up controller, route and form to create new concerts

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -2,5 +2,20 @@ class ConcertsController < ApplicationController
 	def index
 		@concerts = Concert.all
 	end
-
+	def new
+  		@concert = Concert.new 
+	end
+	def create 
+  		@concert = Concert.new(concert_params) 
+  		if @concert.save 
+    		redirect_to '/concerts' 
+  		else 
+    		render 'new' 
+  		end 
+	end
+	
+	private 
+  	def concert_params 
+    	params.require(:concert).permit(:title, :time, :date, :venue, :band, :description, :image_url) 
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root'pages#home'
   get '/concerts' => 'concerts#index' 
+  get 'concerts/new' => 'concerts#new'
+  post 'concerts' => 'concerts#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
