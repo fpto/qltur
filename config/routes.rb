aeaa3b1aa6ef3ea7532d13a1520e8ac173743037
Fabricio Puerto <fabricio.puerto@outlook.com>
2017-02-02 21:51:01 +0100

    Configure users, signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,12 @@ Rails.application.routes.draw do
   get '/venues/:id' => 'venues#show', as: :venue
   post 'venues' => 'venues#create'
 
+  get 'signup'  => 'users#new' 
+  resources :users
 
-
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201201633) do
+ActiveRecord::Schema.define(version: 20170202184134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20170201201633) do
   end
 
   add_index "concerts", ["venue_id"], name: "index_concerts_on_venue_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "venues", force: :cascade do |t|
     t.string   "name"
