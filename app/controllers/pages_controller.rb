90d608b818c8ff0022cf6af5d85c7aa7eb51405a
Fabricio Puerto <fabricio.puerto@outlook.com>
2017-02-19 22:23:09 +0100

    Repaired this week view in the home view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
 	def home
+		#Saving all concerts in thisWeekConcerts to be displayed in the home
 		@concerts = Concert.all
-		range = (DateTime.now..(DateTime.now + 7.days))
+		range = (DateTime.now..(DateTime.now + 6.days))
 		@thisWeekConcerts = @concerts.select{|concert| range.cover?(concert.date)}
 	end
 	def imprint
