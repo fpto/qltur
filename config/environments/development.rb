3c75b315f0bd79db49267fc6844d03fc6cb22f39
Fabricio Puerto <fabricio.puerto@outlook.com>
2017-03-02 16:47:36 +0100

    Improved new concert form with date and time pickers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,9 @@ Rails.application.configure do
   config.assets.digest = true
 
   # Adds additional error checking when serving assets at runtime.
-  # Checks for improperly declared sprockets dependencies.
+  # Checks for improperly declared sprocket≈‚s dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
